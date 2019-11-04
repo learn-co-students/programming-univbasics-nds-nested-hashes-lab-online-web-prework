94c27ce647828942fdaeceb5d2a16b9ef24977fc
Ayako Wiltshire <awittyl44@gmail.com>
2019-11-04 23:03:47 +0000

    Done.

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,19 +7,41 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
+  result = [
+    DON_G,
+    {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"},
+    {:name=>"Pat Monteseian", :occupation=>"Staff"},
+    {:name=>"Kate Gompert", :occupation=>"None"},
+    {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}
+    ]
+ 
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
+  return result
 end
 
 def literal_aoh
+  result = [
+       {:name=>"Don Gately", :occupation=>"Live-in Staff"},
+       {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"},
+       {:name=>"Pat Monteseian", :occupation=>"Staff"},
+       {:name=>"Kate Gompert", :occupation=>"None"},
+       {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}
+       ]
+  
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  return result
 end
 
 def aoh_lookup(aoh, row, key)
+  assembled_aoh[2][:name]
 end
 
 def aoh_update(aoh, row, key, new_value)
+  local_copy_of_aoh = assembled_aoh.dup
+  local_copy_of_aoh[1][:name] = "Joelle van Dyne (PGOAT)"
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
+   p result = local_copy_of_aoh
 end
